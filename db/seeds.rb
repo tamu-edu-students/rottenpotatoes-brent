1ac0d1cc91e0250c83c879d094cb109f6b6f6f4f
renbrent <renbrent23@gmail.com>
2023-09-21 15:19:51 -0500

    added new movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,12 @@ more_movies = [
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
       :release_date => '13-Aug-2021'}
+    {:title => 'Saving Private Ryan', :rating => 'R',
+      :release_date => '24-Jul-1998'}
+    {:title => 'Apollo 13', :rating => 'PG',
+      :release_date => '30-Jun-1995'}
+    {:title => 'Suzume', :rating => 'PG',
+      :release_date => '14-Apr-2023'}
   ]
   
   more_movies.each do |movie|
