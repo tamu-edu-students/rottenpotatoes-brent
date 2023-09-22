5a87710926a254269e80bb6ec0626e54b8d4694c
renbrent <renbrent23@gmail.com>
2023-09-22 11:58:17 -0500

    fixed array and added new movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,15 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
     {:title => 'Saving Private Ryan', :rating => 'R',
-      :release_date => '24-Jul-1998'}
+      :release_date => '24-Jul-1998'},
     {:title => 'Apollo 13', :rating => 'PG',
-      :release_date => '30-Jun-1995'}
+      :release_date => '30-Jun-1995'},
     {:title => 'Suzume', :rating => 'PG',
-      :release_date => '14-Apr-2023'}
+      :release_date => '14-Apr-2023'},
+    {:title => 'Demon Slayer the Movie: Mugen Train', :rating => 'R',
+      :release_date => '23-Apr-2021'}
   ]
   
   more_movies.each do |movie|
